3cceac28303b49da27e48b1f0e0d90327ef32235
Shivam awasthi <90971550+ShivamAwasthi1129@users.noreply.github.com>
2024-08-22 12:44:46 +0530

    pta nhi kya changes hai

--- components/client/SessionProvider.jsx
+++ components/client/SessionProvider.jsx
@@ -1,7 +1,12 @@
 'use client'
-import { SessionProvider as SP } from 'next-auth/react'
+import { SessionProvider as SP } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 function SessionProvider({ children, session }) {
+  // const router = useRouter();
+  // if(!session){
+  //   return router.push("/login");
+  // }
   return (
     <SP session={session}>
       {children}
