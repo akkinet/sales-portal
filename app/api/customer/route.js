ca015deb52dff707c9be0209ede71e1093b7766a
akkinet <akashsharma.zak@gmail.com>
2024-08-29 11:49:01 +0530

    customer list

--- app/api/customer/route.js
+++ app/api/customer/route.js
@@ -2,19 +2,32 @@ const { NextResponse } = require("next/server");
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-
 export const POST = async (req) => {
-    try {
-        const { name, email, address, phone } = await req.json();
-        const customer = await stripe.customers.create({
-            name,
-            email,
-            phone,
-            address: JSON.parse(address)
-        });
+  try {
+    const { name, email, address, phone } = await req.json();
+    const customer = await stripe.customers.create({
+      name,
+      email,
+      phone,
+      address: JSON.parse(address),
+    });
 
-        return NextResponse.json(customer, { status: 200 });
-    } catch (error) {
-        return new Response(JSON.stringify({ error: error.message }), { status: 500 });
-    }
-}
+    return NextResponse.json(customer, { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+};
+
+export const GET = async (req) => {
+  try {
+    const customers = await stripe.customers.list();
+
+    return NextResponse.json(customers, { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+};
--- middleware.js
+++ middleware.js
@@ -1,27 +1,24 @@
 import { NextResponse } from "next/server";
-// import { getSession } from "next-auth/react";
-// import { authOptions } from "./app/utils/authOptions";
+import { getSession } from "next-auth/react";
+import { authOptions } from "./app/utils/authOptions";
 
 export async function middleware(req) {
-  // const session = await getSession(authOptions);
   console.log("Request Method:", req.method);
   // console.log('Request URL:', req.url);
   console.log("Request Query:", req.query);
   const parsedURL = new URL(req.url);
   console.log("Request pathname:", parsedURL.pathname);
-  const domain = req.headers.get('host');
+  const domain = req.headers.get("host");
   const protocol = req.nextUrl.protocol;
   const response = NextResponse.next();
-  response.headers.set('x-domain', domain);
-  response.headers.set('x-protocol', protocol);
-  response.headers.set('x-hostname', `${protocol}//${domain}`);
-
-  // if (!parsedURL.pathname.startsWith("/login") && !session) {
-  //   console.log("sess", session);
-  //   // return NextResponse.json({ error: "unauthenticated" }, { status: 400 });
-  //   return NextResponse.redirect(new URL("/login", req.url))
+  response.headers.set("x-domain", domain);
+  response.headers.set("x-protocol", protocol);
+  response.headers.set("x-hostname", `${protocol}//${domain}`);
+  // if (parsedURL.pathname.startsWith("/api")) {
+  //   const session = await getSession(authOptions);
+  //   if (!session) return NextResponse.redirect(new URL("/login", req.url));
   // }
-  return response
+  return response;
 }
 
 export const config = {
@@ -34,6 +31,6 @@ export const config = {
     // "/products/:path*",
     // "/stripe",
     // "/suits",
-    "/:path*"
+    "/:path*",
   ],
 };
