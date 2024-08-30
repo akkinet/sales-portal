dd7efda66f206e556dedd112a0967c53e08d7a0e
akkinet <akashsharma.zak@gmail.com>
2024-08-30 11:19:07 +0530

    data trimmed

--- app/api/coupon/route.js
+++ app/api/coupon/route.js
@@ -4,7 +4,12 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export const GET = async (req) => {
   try {
-    const coupons = await stripe.coupons.list();
+    let coupons = await stripe.coupons.list();
+    coupons = coupons.data.map((d) => ({
+      id: d.id,
+      name: d.name,
+      percent_off: d.percent_off,
+    }));
     return NextResponse.json(coupons, { status: 200 });
   } catch (error) {
     return NextResponse.json(
