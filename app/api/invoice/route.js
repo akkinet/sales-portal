a5e303510737be7bdd664360db16fad89434e5c7
akkinet <akashsharma.zak@gmail.com>
2024-08-30 15:29:00 +0530

    button disable until resp

--- app/api/invoice/route.js
+++ app/api/invoice/route.js
@@ -146,7 +146,7 @@ export const POST = async (req) => {
                 <p><strong>Due Date:</strong> ${new Date(
                   sentInvoice.due_date * 1000
                 ).toLocaleString()}</p>
-                <p><strong>Amount Due:</strong> ${
+                <p><strong>Amount Due:</strong>$${
                   parseInt(sentInvoice.amount_due) / 100
                 }</p>
             </div>
